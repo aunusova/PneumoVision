5c5dad8b0e259218134f978e74a4fcb72b496be9
aunusova <49620472+aunusova@users.noreply.github.com>
2020-06-23 03:18:04 +0500

    Added changes

--- app/server.py
+++ app/server.py
@@ -38,8 +38,8 @@ def analyze():
     if request.method == 'GET':
 	    return render_template('index.html')
     else:
-        img_data = await request.form()
-        img_bytes = await (img_data['file'].read())
+        img_data = request.form()
+        img_bytes = img_data['file'].read()
         img = open_image(BytesIO(img_bytes))
         prediction = learn.predict(img)[0]
     return render_template('result.html', label = prediction, image_path = img)
