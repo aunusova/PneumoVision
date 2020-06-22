1fe091692b596001c6bbef286cb802ce659c2a41
aunusova <49620472+aunusova@users.noreply.github.com>
2020-06-22 07:06:10 +0500

    "A lot of new features"

--- app/static/index.js
+++ app/static/index.js
@@ -14,6 +14,29 @@ function showPicked(input) {
     reader.readAsDataURL(input.files[0]);
   }
 
+function analyze() {
+    var uploadFiles = el("file-input").files;
+    if (uploadFiles.length !== 1) alert("Please select a file to analyze!");
+  
+    var xhr = new XMLHttpRequest();
+    var loc = window.location;
+    xhr.open("POST", `${loc.protocol}//${loc.hostname}:${loc.port}/analyze`,
+      true);
+    xhr.onerror = function() {
+      alert(xhr.responseText);
+    };
+    xhr.onload = function(e) {
+      if (this.readyState === 4) {
+        var response = JSON.parse(e.target.responseText);
+        el("result-label").innerHTML = `Result = ${response["result"]}`;
+      }
+    };
+  
+    var fileData = new FormData();
+    fileData.append("file", uploadFiles[0]);
+    xhr.send(fileData);
+}
+
     this.state = {
         file: null,
         predictions: [],
